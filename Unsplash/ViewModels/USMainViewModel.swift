30b0212813650810b40029fd4e510257ddf4fb14
luthfifr <luthfir272@gmail.com>
2020-02-19 15:38:19 +0700

    set  and instantiate main viewcontroller

---
+++ Unsplash/ViewModels/USMainViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  USMainViewModel.swift
+//  Unsplash
+//
+//  Created by Luthfi Fathur Rahman on 19/02/20.
+//  Copyright © 2020 StandAlone. All rights reserved.
+//
+
+import Foundation
+
+final class CCMainViewModel: NSObject {
+    
+}
---
+++ Unsplash/Views/USMainViewController.swift
@@ -0,0 +1,47 @@
+//
+//  USMainViewController.swift
+//  Unsplash
+//
+//  Created by Luthfi Fathur Rahman on 19/02/20.
+//  Copyright © 2020 StandAlone. All rights reserved.
+//
+
+import UIKit
+
+class USMainViewController: UIViewController {
+
+    private var viewModel: CCMainViewModel!
+
+    // MARK: - Initialization
+    convenience init() {
+        self.init(viewModel: nil)
+    }
+
+    init(viewModel: CCMainViewModel?) {
+        super.init(nibName: nil, bundle: nil)
+
+        self.viewModel = viewModel
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
